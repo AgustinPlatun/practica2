3c96a4ca5e5ab31daae6e98ff391eedb813b0bb2
AgustinPatun <agusplatun@hotmail.com>
2024-04-11 23:40:58 -0300

    Realizacion del punto 3

--- modules/functions.py
+++ modules/functions.py
@@ -1,9 +1,16 @@
-def goals_maximun(players):
-    maximun = -1
-    aux = None
+#Saca el maximo recibiendo una lista de tuplas como parametro
+def maximum(players):
+    max = -1
     for player in players:
-        if(player[1] > maximun):
-            maximun = player[1]
+        if(player[1] > max):
+            max = player[1]
             aux =(player[0],player[1])
+    return aux
+
+#Calcula la efectividad de l@s jugador@s
+def effectiveness(data):
+    aux = []
+    for player in data:
+        aux.append((player[0],(player[1] * 1.5) + (player[2] * 1.25) + player[3]))
     return aux
 
